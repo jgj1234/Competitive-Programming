086b3786102b26e6562e0a72c8d180aaf7f55279
JGJCode <jensengjacob@gmail.com>
2025-01-01 11:47:06 -0500

    Added Solution to 449D - Jzzhu and Numbers

---
+++ Codeforces/449D.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define pii pair<int,int>
+#define pb push_back
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define MOD 1000000007
+#define all(x) x.begin(),x.end()
+#define MOD2 998244353
+using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+template<int MODX>
+struct ModInt {
+  unsigned x;
+  ModInt() : x(0) { }
+  ModInt(signed sig) : x(((sig%MODX)+MODX)%MODX) {  }
+  ModInt(signed long long sig) : x(((sig%MODX)+MODX)%MODX) { }
+  int get() const { return (int)x; }
+  ModInt pow(ll p) { ModInt res = 1, a = *this; while (p) { if (p & 1) res *= a; a *= a; p >>= 1; } return res; }
+ 
+  ModInt &operator+=(ModInt that) { if ((x += that.x) >= MODX) x -= MODX; return *this; }
+  ModInt &operator-=(ModInt that) { if ((x += MODX - that.x) >= MODX) x -= MODX; return *this; }
+  ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MODX; if (x < 0) x += MODX; return *this; }
+  ModInt &operator/=(ModInt that) { return (*this) *= that.pow(MODX - 2); }
+ 
+  ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+  bool operator<(ModInt that) const { return x < that.x; }
+  friend ostream& operator<<(ostream &os, ModInt a) { os << a.x; return os; }
+};
+typedef ModInt<MOD> mint;
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+void solve(int cas){  
+    int n; cin>>n;
+    vector<ll> nums(n);
+    for (auto& a: nums) cin>>a;
+    vector<int> dp(1<<20);
+    for (int i = 0; i < n; i++){
+        dp[nums[i]]++;
+    }
+    for (int i = 0; i < 20; i++){
+        for (int mask = 0; mask < (1<<20); mask++){
+            if ((mask&(1<<i))==0) dp[mask] += dp[mask|(1<<i)];
+        }
+    }
+    mint res = binpow(2, n, MOD) - 1;
+    for (int mask = 1; mask < (1<<20); mask++){
+        if (__builtin_popcount(mask)%2==1){
+            res-=binpow(2,dp[mask], MOD)-1;
+        }
+        else{
+            res+=binpow(2,dp[mask],MOD)-1;
+        }
+    }
+    cout<<res<<endl;
+}
+int main(){
+    int t=1;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //cin>>t;
+    for (int i = 1; i<=t; i++){
+        solve(i);
+    }
+    return 0;
+}
